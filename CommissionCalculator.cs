b9d5de8b975a78162c99f2088463cce04006d6d6
GaborIreHun <76515173+GaborIreHun@users.noreply.github.com>
2021-01-16 21:04:11 +0000

    Add files via upload

---
+++ CommissionCalculator.cs
@@ -0,0 +1,17 @@
+﻿
+class CommissionCalculator
+{
+    public decimal soldItem;
+
+    public void SetSoldItem(decimal commissionCalculatorSoldItem)
+    {
+        soldItem = commissionCalculatorSoldItem;
+    }
+
+    public decimal GetSoldItem()
+    {
+        return soldItem;
+    }
+
+}
+
---
+++ CommissionCalculatorTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+class CommissionCalculatorTest
+{
+    static void Main()
+    {
+        CommissionCalculator commissionCalculator = new CommissionCalculator();
+
+
+        int soldItemCounter = 1;
+
+        decimal total = 0;
+
+        double commission = 0.09;
+
+        decimal wage = 200;
+
+
+        while(true)
+        {
+            Console.Write($"Please enter the value of sold item {soldItemCounter} or -1 to quit: ");
+            decimal commissionCalculatortheSoldItem = decimal.Parse(Console.ReadLine());
+            commissionCalculator.SetSoldItem(commissionCalculatortheSoldItem);
+
+
+            ++soldItemCounter;
+
+            total = (decimal)(commissionCalculator.GetSoldItem() + total);
+
+            decimal totalToPay = total * (decimal)commission + wage;
+
+
+            if (commissionCalculatortheSoldItem == -1)
+            {
+                Console.WriteLine($"\nThe total value of the sold item(s) is: {total}");
+                Console.WriteLine($"\nThe weekly salary with commission is: {totalToPay}\n");
+                break;
+            }
+
+        }
+
+        Console.WriteLine("\nThank you and see you soon!");
+
+    }
+}
+
